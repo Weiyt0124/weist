51c52aa0fa12f4a5d9c4b5aee1e6826855c1ef85
Weiyt <75768623@qq.com>
2017-09-11 16:07:30 +0800

    修改注释

--- src/main/java/com/wyt/service/impl/MessageBoardServiceImpl.java
+++ src/main/java/com/wyt/service/impl/MessageBoardServiceImpl.java
@@ -23,7 +23,7 @@ public class MessageBoardServiceImpl implements MessageBoardService {
 
 
     /**
-     *  查询回复列表权重排序:5
+     *  查询回复列表权重排序:10
      *  @Author Wyt
      *
      *  @Date 2017/9/11 14:22
